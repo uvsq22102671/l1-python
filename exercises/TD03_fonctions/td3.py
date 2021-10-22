fd841da98ce6f48e8ffaddc097f310ac1790c0f5
Clara CHHIN <clara.chhin@ens.uvsq.fr>
2021-10-22 09:33:25 +0200

    td2

--- exercises/TD03_fonctions/td3.py
+++ exercises/TD03_fonctions/td3.py
@@ -2,16 +2,14 @@ def tempsEnSeconde(temps):
     tempsEnSeconde = temps[0]*24*3600 + temps[1]*3600 + temps[2]*60 + temps[3]
     return tempsEnSeconde
 
-temps = (3,23,1,34)
-print(type(temps))
-print(tempsEnSeconde(temps))   
-
 def secondeEnTemps(seconde):
     temps[1] = (seconde%86400)//3600
     temps[2] = ((seconde%86400)%3600)//60
     temps[3] = (seconde%86400)%3600)%60)
-    return temps
-
+    return secondeEnTemps
 
 temps = secondeEnTemps(100000)
 print(temps[0],"jours",temps[1],"heures",temps[2],"minutes",temps[3],"secondes")
+
+def afficheTemps(temps):
+    print (temps[0],"jours")
